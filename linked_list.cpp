2fc146e282e824aa0e8b6200d90f244cbd534c46
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-11-12 22:34:12 +0530

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -2,41 +2,88 @@
 using namespace std;
 
 struct node{
-    int data;
+    struct node *prev;
+    int roll_no;
+    char name[20];
+    int year;
+    char section:
+    char from[20];
     struct node * next;
-}*first=NULL;
+}*start=NULL;
 
+typedef struct node node;
 
-void create(int arr[],int n){
-    int i;
-    first=new node();
-    struct node *t,*last;
-    first->data=arr[0];
-    first->next=NULL;
-    last =first;
-
-    for (int i = 1; i < n; i++)
-    {
-        /* code */  
-        t=new node();
-        t->data=arr[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
-
-    }
+node* create(){
+    node * newnode;
+    newnode=(node*)malloc(sizeof(node));
+    cout<<"Enter roll no\n";
+    cin>>newnode->roll_no;
+    cout<<"Enter Name\n";
+    gets(newnode->name);
+    cout<<"Enter year\n";
+    cin>>newnode->year;
+    cout<<"Enter section\n";
+    cin>>newnode->section;
+    cout>>"Enter which state you came from\n";
+    gets(newnode->from);
+    return newnode;
     
 }
 
-void display(struct node *p){
-    while (p!=NULL)
-    {
-        /* code */
-        cout<<p->data;
-        p=p->next;
-    }
+void create_a_list_of_n_node(int n){
     
 }
+
+void insert_at_begining(){
+
+    node newnode=create();
+    if(start){
+        start=newnode();
+    }
+    else{
+        start->prev=newnode;
+        newnode->prev=start;
+        newnode->next=start;
+        start=newnode;
+    }
+}
+void insert_at_end{
+    node newnode=create();
+    node* temp;
+    temp=start;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newnode;
+}
+void insert_at_mid(){
+    if(start){
+        cout<<"Empty Record";
+        return;
+    }
+    else{
+        node newnode=create();
+        node *temp=start;
+        int temp_roll_no;
+        cout>>"Enter roll no\n";
+        cin>>temp_roll_no;
+
+        while (temp->roll_no!=temp_roll_no)
+        {
+            temp=temp->next;
+        }
+        temp->next->prev=newnode;
+        nenode->next=temp->next;
+        temp->next=nennode;
+        newnode->prev=temp;
+    }   
+}
+
+void
+   
+
+    
+
 
 int main(){
 #ifndef ONLINE_JUDGE
@@ -44,9 +91,7 @@ int main(){
     freopen("output.txt", "w", stdout);
 #endif
     
-    int arr[]={1,5,3,6,7,8};
-    create(arr,6);
-    display(first);
+    
     
     return 0;
 }
